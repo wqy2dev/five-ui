88060563c1933428aa2c0c089c0a5d49ad52822b
Chris <wqynqa@163.com>
2024-12-02 20:48:10 +0800

    chore: example

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,13 +6,13 @@ let { children } = $props();
 </script>
 
 <div class="flex flex-row w-screen h-screen">
-    <div class="w-64 bg-slate-50 pl-2 text-sm">
-        <a class="block w-full leading-8" href="/alert">Alert</a>
-        <a class="block w-full leading-8" href="/button">Button</a>
-        <a class="block w-full leading-8" href="/card">Card</a>
-        <a class="block w-full leading-8" href="/tooltip">Tooltip</a>
+    <div class="w-56 bg-slate-50 text-sm shrink-0">
+        <a class="block w-full leading-8 pl-2 hover:bg-primary-100" href="/alert">Alert</a>
+        <a class="block w-full leading-8 pl-2 hover:bg-primary-100" href="/button">Button</a>
+        <a class="block w-full leading-8 pl-2 hover:bg-primary-100" href="/card">Card</a>
+        <a class="block w-full leading-8 pl-2 hover:bg-primary-100" href="/tooltip">Tooltip</a>
     </div>
-    <div class="grow shrink-0 p-5">
+    <div class="grow p-5">
         {@render children()}
     </div>
 </div>
