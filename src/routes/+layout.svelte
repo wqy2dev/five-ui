826bd6e823b6f6885ffe36b216497364a71d35c6
van <wqynqa@163.com>
2024-12-12 11:40:32 +0800

    chore: add example

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,29 @@
 <script lang="ts">
+import { Badge } from "$lib/index.js";
+
 import "./style.css";
 
 let { children } = $props();
 
 const routes = [
     {path: "/accordion", title: "Accordion"},
-    {path: "/alert", title: "Alert"},
-    {path: "/avatar", title: "Avatar"},
-    {path: "/badge", title: "Badge"},
-    {path: "/breadcrumb", title: "Breadcrumb"},
-    {path: "/button", title: "Button"},
+    {path: "/alert", title: "Alert", achieve: true},
+    {path: "/avatar", title: "Avatar", achieve: true},
+    {path: "/badge", title: "Badge", achieve: true},
+    {path: "/breadcrumb", title: "Breadcrumb", achieve: true},
+    {path: "/button", title: "Button", achieve: true},
     {path: "/calendar", title: "Calendar"},
     {path: "/card", title: "Card"},
     {path: "/carousel", title: "Carousel"},
     {path: "/cascader", title: "Cascader"},
     {path: "/datepicker", title: "DatePicker"},
-    {path: "/drawer", title: "Drawer"},
+    {path: "/drawer", title: "Drawer", achieve: true},
     {path: "/form", title: "Form"},
-    {path: "/icon", title: "Icon"},
+    {path: "/icon", title: "Icon", achieve: true},
     {path: "/input", title: "Input"},
-    {path: "/layout", title: "Layout"},
+    {path: "/layout", title: "Layout", achieve: true},
     {path: "/message", title: "Message"},
-    {path: "/modal", title: "Modal"},
+    {path: "/modal", title: "Modal", achieve: true},
     {path: "/notification", title: "Notification"},
     {path: "/pagination", title: "Pagination"},
     {path: "/radio", title: "Radio"},
@@ -29,13 +31,13 @@ const routes = [
     {path: "/slider", title: "Slider"},
     {path: "/steps", title: "Steps"},
     {path: "/switch", title: "Switch"},
-    {path: "/table", title: "Table"},
+    {path: "/table", title: "Table", achieve: true},
     {path: "/tabs", title: "Tabs"},
-    {path: "/tag", title: "Tag"},
+    {path: "/tag", title: "Tag", achieve: true},
     {path: "/timeline", title: "Timeline"},
     {path: "/timepicker", title: "TimePicker"},
-    {path: "/tooltip", title: "Tooltip"},
-    {path: "/watermark", title: "WaterMark"},
+    {path: "/tooltip", title: "Tooltip", achieve: true},
+    {path: "/watermark", title: "WaterMark", achieve: true},
 ];
 
 </script>
@@ -43,7 +45,20 @@ const routes = [
 <div class="flex flex-row w-screen h-screen">
     <div class="w-56 h-full bg-slate-100 text-sm shrink-0 overflow-y-auto">
         {#each routes as route}
-            <a class="block w-full pl-6 py-2 hover:bg-primary-100" href={route.path}>{route.title}</a>
+            <a class="flex flex-row w-full pl-6 py-2 hover:bg-primary-100" href={route.path}>
+                <span class="grow">
+                    {route.title}
+                </span>
+
+                {#if route.achieve}
+                    <span class="ml-auto text-red-600 px-3">
+                        <Badge 
+                            variant="pink"
+                            content="✔"
+                        />
+                    </span>
+                {/if}
+            </a>
         {/each}
     </div>
     <div class="relative grow py-5 px-8 overflow-y-auto">
