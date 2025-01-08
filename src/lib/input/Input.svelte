b73c07ee31ca48768353ceed549bd29c4e678c06
van <wqynqa@163.com>
2025-01-08 16:31:12 +0800

    update input

--- src/lib/input/Input.svelte
+++ src/lib/input/Input.svelte
@@ -1,47 +1,200 @@
-<script lang="ts">
-import type { Snippet } from "svelte";
-import type { FullAutoFill, KeyboardEventHandler } from "svelte/elements";
+<script lang="ts" module>
+import { onMount, type Snippet } from "svelte";
+import { type VariantProps, tv } from "tailwind-variants";
+import { Close } from "$lib/icons/index.js";
+import type { FullAutoFill } from "svelte/elements";
 
-interface InputProps {
-	id?: string;
-	name?: string;
-	value?: string|number;
-    type?: "text" | "password" | "number";
-	size?: "default" | "sm" | "lg";
-    class?: string;
-	width?: number | string;
-    disabled?: boolean;
-	minlength?: number;
-	maxlength?: number;
-	placeholder?: string;
-	autocomplete?: FullAutoFill;
-	readonly?: boolean;
-	head?: Snippet;
-	tail?: Snippet;
-	ref?: {(el:HTMLElement):void};
-	oninput?: {(value?:string):void};
-	onchange?: {(value?:string):void};
-	onkeypress?: {(code:string):void};
+const inputVariants = tv({
+	slots: {
+		base: "w-full align-top inline-flex flex-row items-center px-2 rounded-md bg-white border border-solid border-slate-200 overflow-hidden transition",
+		input: "grow shrink min-w-5 text-sm text-slate-700 disabled:text-slate-400 placeholder:text-slate-400 bg-transparent cursor-inherit outline-none",
+		slot: "w-5 shrink-0 flex items-center justify-center text-slate-600 z-1 cursor-inherit",
+	},
+	variants: {
+		disabled:{
+			true: {
+				base: "cursor-not-allowed bg-slate-50",
+			}
+		},
+		focus: {
+			true: {
+				base: "ring-offset-1 ring-2 ring-primary",
+			},
+		},
+		size: {
+			sm: {
+				base: "h-8 leading-8",
+				slot: "h-8",
+			},
+			md: {
+				base: "h-9 leading-9",
+				slot: "h-9",
+			},
+			lg: {
+				base: "h-10 leading-10",
+				slot: "h-10",
+			},
+		},
+		slot: {
+			head: {
+				slot: "justify-start",
+			},
+			tail: {
+                slot: "justify-end",
+			},
+			erase: {
+				slot: "justify-end cursor-pointer",
+			},
+		},
+	},
+	defaultVariants: {
+		size: "md",
+	},
+});
+
+type Size = VariantProps<typeof inputVariants>["size"];
+
+type InputProps = {
+	id?:string;
+	class?:string;
+	size?:Size;
+	name?:string;
+	value?:string|number;
+    type?:"text"|"password"|"number";
+	minlength?:number;
+	maxlength?:number;
+	placeholder?:string;
+	autocomplete?:FullAutoFill;
+	readonly?:boolean;
+    disabled?:boolean;
+	head?:Snippet;
+	tail?:Snippet;
+	ref?:{(el:HTMLElement):void};
+	oninput?:{(value?:string):void};
+	onchange?:{(value?:string):void};
+	onkeypress?:{(code:string):void};
+	// input number
+	min?:number;
+	max?:number;
+	step?:number;
 }
+	
+</script>
+
+<script lang="ts">
 
 let {
+	id,
     head,
     tail,
     name,
+    class:className,
     value,
-    width,
     size,
     disabled,
-    class: className,
     oninput,
     onchange,
+	onkeypress,
     ref,
     ...restProps
 }:InputProps = $props();
 
+let el:HTMLElement;
+let inner:HTMLInputElement;
+
+onMount(() => {
+    ref?.(el);
+});
+
+// const fieldContext = getContext<FormFieldContext>("tc-form-field");
+// if(fieldContext) {
+//     name = fieldContext.name;
+//     value = fieldContext.value;
+//     onchange = fieldContext.onchange;
+// }
+
+let focus = $state(false);
+let hover = $state(false);
+
+// erase input
+function onErase(_:MouseEvent) {
+    value = "", focus = true;
+
+	if(inner) {
+		inner.focus();
+	}
+
+    if(onchange) {
+        onchange("");
+    }
+}
+
+function onFocus(e:FocusEvent) {
+    if(!disabled) {
+        focus = e.type === "focus";
+    }
+}
+
+function onHover(e:Event) {
+    hover = e.type === "mouseenter";
+}
+
+function onInput(e:Event & {currentTarget: EventTarget & HTMLInputElement}) {
+    if(e.type === "input") {
+        oninput?.(e.currentTarget.value);
+    }
+    else if(e.type === "change") {
+        onchange?.(e.currentTarget.value);
+    }
+}
+
+const {
+	base,
+	slot,
+	input,
+} = inputVariants();
+
 </script>
 
-<div 
+<div
+	bind:this={el}
+	id={id}
+	role="form"
+	class={base({size, focus:focus && !disabled, disabled, className})}
+    onmouseenter={onHover}
+    onmouseleave={onHover}
+    {...{size}}
 >
+	{#if head}
+		<div class={slot({slot:"head"})}>
+			{@render head()}
+		</div>
+	{/if}
 
+	<input
+		bind:this={inner}
+		bind:value={value}
+		class={input()}
+		name={name}
+		disabled={disabled}
+		onfocus={onFocus}
+		onblur={onFocus}
+		oninput={onInput}
+		onchange={onInput}
+		{...restProps}
+	/>
+
+	{#if !disabled && hover && value }
+		<div class={slot({slot:"erase"})}>
+			<button onmouseup={onErase}>
+				<Close size={15}/>
+			</button>
+		</div>
+	{/if}
+
+	{#if tail}
+        <div class={slot({slot:"tail"})}>
+	        {@render tail()}
+		</div>
+	{/if}
 </div>
