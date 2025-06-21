39995f25ac1a1bd8134436fdcf4b4b25582897b1
Chris <wqynqa@163.com>
2025-06-21 19:07:57 +0800

    optimize: button & modal

--- src/lib/button/Button.svelte
+++ src/lib/button/Button.svelte
@@ -23,10 +23,10 @@ const buttonVariants = tv({
             none: "",
         },
         size: {
-            xs: "px-2 py-0.5 text-xs",
-            sm: "px-3 py-1.5 text-sm",
-            md: "px-4 py-2 text-sm",
-            lg: "px-5 py-2.5 text-base",
+            xs: "px-1.5 py-0.5 text-xs",
+            sm: "px-2 py-1 text-sm",
+            md: "px-3.5 py-1.5 text-sm",
+            lg: "px-4 py-2 text-base",
         },
     },
     defaultVariants: {
